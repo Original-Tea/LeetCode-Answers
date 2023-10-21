53b90e54e396d323f49cec6ded9056f2b26dd9df
Original-Tea <125925424+Original-Tea@users.noreply.github.com>
2023-10-21 13:39:36 +0300

    Time: 49 ms (70.48%), Space: 42 MB (65.1%) - LeetHub

--- 0100-same-tree/0100-same-tree.js
+++ 0100-same-tree/0100-same-tree.js
@@ -12,48 +12,14 @@
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
-    let stackp = [];
-    let stackq = [];
-
-    let posp = p;
-    let posq = q;
-
-    if(!posp) {
-        if(!posq) {
-            return true;
-        } else {
-            return false;
-        }
-    } else if(!posq) {
-        if(!posp) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    while((posp || stackp.length) && (posq || stackq.length)) {
-        if(posp && posq) {
-            stackp.push(posp);
-            stackq.push(posq);
-            if(posp.val == posq.val) {
-                posp = posp.left;
-                posq = posq.left;
-            } else {
-                return false;
-            }
-        } else if(!posp && !posq) {
-            posp = stackp.pop();
-            posp = posp.right;
-            posq = stackq.pop();
-            posq = posq.right;
-        } else {
-            return false;
-        }
-    }
-
-    if(posq == posp) {
+    if(!p && !q) {
         return true;
+    } else if(!p || !q) {
+        return false;
+    }
+
+    if(p.val == q.val) {
+        return (isSameTree(p.left, q.left) && isSameTree(p.right, q.right))
     } else {
         return false;
     }
