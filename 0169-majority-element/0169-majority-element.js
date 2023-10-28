1f74e03b86b6b47f69b20e6f283f9edae039cf40
Original-Tea <125925424+Original-Tea@users.noreply.github.com>
2023-10-28 16:28:50 +0300

    Time: 63 ms (35.53%), Space: 42.5 MB (99.01%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -3,7 +3,22 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    nums = nums.sort((a, b) => a - b);
+    let candidate = 0;
+    let count = 0;
 
-    return nums[Math.floor(nums.length / 2)]
+    for(let i = 0; i < nums.length; i++) {
+        if(count == 0) {
+            candidate = nums[i];
+            count++;
+            continue;
+        } 
+
+        if(nums[i] == candidate) {
+            count++;
+        } else {
+            count--;
+        }
+    }
+
+    return candidate;
 };
