81f9d4f6bb1940b331ad80c10ff1ea1ece00aa1a
Original-Tea <125925424+Original-Tea@users.noreply.github.com>
2023-11-03 17:26:17 +0200

    Time: 81 ms (38.95%), Space: 46.6 MB (46.96%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -3,13 +3,19 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    let end = nums.length
-    for(let i = 0 ; i < end; i++) {
-        if(nums[i] == 0) {
-            nums.splice(i, 1);
-            nums.push(0);
-            end--;
-            i--;
-        } 
+    let low = 0;
+    let high = low + 1;
+
+    while(high < nums.length) {
+        if(nums[low] !== 0) {
+            low++;
+            high++;
+        } else {
+            if(nums[high] !== 0) {
+                [nums[low] , nums[high]] = [nums[high], nums[low]];
+                low++;
+            }
+            high++;
+        }
     }
 };
